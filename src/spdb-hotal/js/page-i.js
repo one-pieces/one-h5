8abb3c9cef0a4d1a882c87f25807ba22d38fe2e0
林小龙 <xiaolong.lin@ele.me>
2018-04-10 02:36:35 +0800

    fix: 去掉泳池对话框

--- src/spdb-hotal/js/page-i.js
+++ src/spdb-hotal/js/page-i.js
@@ -2,16 +2,15 @@ function PageI (element, callback) {
   // 根元素
   this.$root = element;
 
-  this.$talk = element.find('.page-i__talk');
   this.$coin = element.find('.page-i__coin');
 
   this.callback = callback;
 }
 
 PageI.prototype.run = function() {
-  this.$coin.show();
   return new Promise(resolve => {
-    this.$talk.fadeIn(500, () => {
+    this.$coin.show();
+    setTimeout(() => {
       this.$coin
         .css({ transition: 'all 1.5s ease-in-out'})
         .css({ top: '70%' })
